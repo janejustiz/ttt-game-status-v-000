c25027929cbee145ece7dc00183c2b96de40c1d7
Jane Justiz <janejustiz@gmail.com>
2018-05-31 00:40:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ end
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-  win_combination=WIN_COMBINATIONS.each
+  win_combination = WIN_COMBINATIONS.each
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -15,14 +15,13 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination
-    else
-      false
-    end
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return win_combination
+      else
+        false
+      end
 end
 
 def full?
